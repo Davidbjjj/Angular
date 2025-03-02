3a714b0e46675a4c658cebc61f402cef4524af28
davidbjjj <pondavid103@gmail.com>
2025-03-02 18:33:42 -0300

    usando ? para atributos/obj indefinidos

--- src/app/carrosel/carrosel.component.ts
+++ src/app/carrosel/carrosel.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrl: './carrosel.component.css'
 })
 export class CarroselComponent {
-   categoria="Categoria"
-}
+   categoria="Categoria";
+   OBJ={
+    itemsobj:{
+      item1:1,
+      item2:"Item3",
+    },
+   };
+   /**Quando der o erro de Property 'tepy' does not exist on type 'never', é por conta que o não consegue indentificar qual o tipo/tipagem do objeto.plano:any{info:sss,ui:""} */
+   /**ou seja 
+    * 
+    * OBJ:any ={
+    itemsobj:{
+      item1:1,
+      item2:"Item3",
+    },
+   };
+    * 
+    * 
+    * 
+    */
+
+  }
